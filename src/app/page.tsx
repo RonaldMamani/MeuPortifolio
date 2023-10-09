9dbfa4d692fce3bccbc12b7df8c4bb801d62f767
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-10-09 11:31:51 -0300

    Adicionando Seções Sobre e Contato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import Contact from "@/components/Contact/Index";
 import Footer from "@/components/Footer/Index";
 import Header from "@/components/Header/Index";
 import HomePage from "@/components/HomePage/Index";
+import Sobre from "@/components/Sobre/Index";
 
 export default function Home() {
   return (
     <main className="bg-black">
       <Header />
       <HomePage />
+      <Sobre />
+      <Contact />
       <Footer />
     </main>
   )
